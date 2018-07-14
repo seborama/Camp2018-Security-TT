9270dafed0cc2279c5de8f87604ed233928c2bc4
Seb C <seborama@users.noreply.github.com>
2018-07-14 21:46:15 +0100

    Fri13 wip - Merge (#1)
    * Enhanced support for Splunk as a DaemonSet (WIP)
    
    * Fixed registry issues
    
    * Fixed option on helm

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,8 +6,15 @@
 readonly SCRIPT_HOME="$(pushd $(dirname $0)/.. >/dev/null ; echo ${PWD})"
 
 readonly K8S_NAMESPACE="security-tt"
-readonly MINIKUBE_PROFILE="${MINIKUBE_PROFILE:-minikube-security-tt}"
+#readonly MINIKUBE_PROFILE="${MINIKUBE_PROFILE:-minikubesecuritytt}"
+readonly MINIKUBE_PROFILE="minikube"
+[ "${MINIKUBE_PROFILE}" != "minikube" ] &&
+    read -p "Note: using a profile name other than minikube appears to cause stability issues with minikube and kubeadm"
+
 readonly MINIKUBE_VM_DRIVER="${MINIKUBE_VM_DRIVER:-virtualbox}"
+
+readonly OK=0
+readonly NOK=1
 
 
 #####################################################################
@@ -22,7 +29,7 @@ function banner() {
 
 
 #####################################################################
-function install_Brew() {
+function brew_install() {
 #####################################################################
     local package=${1:-Missing package argument in function `$FUNCNAME[0]`}
 
@@ -32,7 +39,7 @@ function install_Brew() {
 
 
 #####################################################################
-function install_BrewCask() {
+function brew_cask_install() {
 #####################################################################
     local package=${1:-Missing package argument in function `$FUNCNAME[0]`}
 
@@ -46,8 +53,8 @@ function install_VirtualBox() {
 #####################################################################
     banner "Installing VirtualBox"
 
-    install_BrewCask virtualbox || exit 1
-    install_BrewCask virtualbox-extension-pack || exit 1
+    brew_cask_install virtualbox || exit 1
+    brew_cask_install virtualbox-extension-pack || exit 1
 }
 
 
@@ -56,8 +63,8 @@ function install_Vagrant() {
 #####################################################################
     banner "Installing Vagrant"
 
-    install_BrewCask vagrant || exit 1
-    install_Brew vagrant-completion || exit 1
+    brew_cask_install vagrant || exit 1
+    brew_install vagrant-completion || exit 1
 }
 
 
@@ -76,30 +83,48 @@ function install_Kali() {
 
 
 #####################################################################
-function build_SplunkForwarderImage() {
+function install_kubernetes_cli() {
 #####################################################################
-    pushd "${SCRIPT_HOME}/SplunkForwarder" >/dev/null || exit 1
+    banner "Installing kubernetes-cli"
 
-    banner "Building Splunk Universal Forwarder Docker image"
-    echo "NOTE: This will be updated to have more relevant inputs.conf"
-
-    docker build -t splunk_fwdr:v1 . | exit 1
-
-    popd >/dev/null || exit 1
+    brew_install kubernetes-cli || exit 1
 }
 
 
 #####################################################################
-function run_SplunkForwarderContainer() {
+function install_Helm() {
 #####################################################################
-    pushd "${SCRIPT_HOME}/SplunkForwarder" >/dev/null || exit 1
+    banner "Installing Helm"
 
-    banner "Running Splunk Universal Forwarder Docker container"
-    echo "NOTE: This will be transferred to K8s ASAP"
+    brew_install kubernetes-helm || exit 1
+}
 
-    docker run --rm --env SPLUNK_START_ARGS="--accept-license" -d splunk_fwdr:latest || exit 1
 
-    popd >/dev/null || exit 1
+#####################################################################
+function install_Minikube() {
+#####################################################################
+    banner "Installing Minikube"
+
+    brew_cask_install minikube || exit 1
+}
+
+
+#####################################################################
+function install_Docker() {
+#####################################################################
+    banner "Installing Docker"
+
+    brew_cask_install docker || exit 1
+    brew_install docker-completion || exit 1
+}
+
+
+#####################################################################
+start_Docker() {
+#####################################################################
+    banner "Starting Minikube"
+
+    open --background -a Docker
 }
 
 
@@ -110,7 +135,9 @@ function createAndRun_Minikube() {
 
     banner "Creating and running Minikube (profile: ${MINIKUBE_PROFILE})"
 
-    if [ -d ~/.minikube/machines/${MINIKUBE_PROFILE}/ ]; then
+    eval $(minikube docker-env --unset) || exit 1
+
+    if [ -d "${installDir}" ]; then
         echo "Minikube has already been set-up previously."
         local choice
         read -p "WARNING - Would you like to DESTROY it? (default: n) " choice
@@ -121,8 +148,13 @@ function createAndRun_Minikube() {
         rm -rf "${installDir}"
     fi
 
-    minikube --profile=${MINIKUBE_PROFILE} start --cache-images --vm-driver=${MINIKUBE_VM_DRIVER} --memory=8192 --cpus=4 || exit 1
+    #  --logtostderr --v=2 --stderrthreshold=2 --loglevel=1
+    minikube --profile=${MINIKUBE_PROFILE} start --vm-driver=${MINIKUBE_VM_DRIVER} --memory=8192 --cpus=4 || exit 1
+    minikube addons enable registry || exit 1
+    minikube addons disable heapster
+    minikube addons disable metrics-server
 }
+
 
 #####################################################################
 function create_K8s_Namespace() {
@@ -132,22 +164,104 @@ function create_K8s_Namespace() {
     kubectl --context=${MINIKUBE_PROFILE} create namespace ${K8S_NAMESPACE}
 }
 
+
+#####################################################################
+#function run_SplunkForwarderContainer() {
+#####################################################################
+#    pushd "${SCRIPT_HOME}/SplunkForwarder" >/dev/null || exit 1
+#
+#    banner "Running Splunk Universal Forwarder Docker container"
+#    echo "NOTE: This will be transferred to K8s ASAP"
+#
+#    docker run --rm --env SPLUNK_START_ARGS="--accept-license" -d splunk_fwdr:latest || exit 1
+#
+#    popd >/dev/null || exit 1
+#}
+
+
+#####################################################################
+function wait_until_k8s_environment_is_ready() {
+#####################################################################
+    local reportFile=/tmp/setup_local.$$
+    local jsonPath='{range .items[*]}±{@.metadata.name}:{range @.status.containerStatuses[*]}ready={@.ready};{end}{end}'
+
+    banner "Waiting for K8s environment readiness"
+
+    while true;
+    do
+        kubectl get pods --all-namespaces -o jsonpath="${jsonPath}" | tr "±" "\n" | grep false >"${reportFile}"
+        [ "$(wc -l ${reportFile} | awk '{print $1}')" != 0 ] && break
+        sleep 2
+    done
+
+    rm "${reportFile}"
+}
+
+
+#####################################################################
+function wait_minikiube_registry_addon_is_ready() {
+#####################################################################
+    banner "Waiting for Minikube registry addon readiness"
+
+    while ! kubectl -n kube-system get svc registry &>/dev/null; do sleep 2 ; done
+
+    # get the ip of the registry endpoint
+    export REGISTRY_CLUSTERIP=$(kubectl -n kube-system get svc registry -o jsonpath="{.spec.clusterIP}") || exit 1
+    echo "Minikube registry Service ClusterIP: ${REGISTRY_CLUSTERIP}"
+
+    while ! minikube ssh "curl -sSI ${REGISTRY_CLUSTERIP}/v2/" | grep -q " 200 OK"; do sleep 2; done
+}
+
+
+#####################################################################
+function build_SplunkForwarderImage() {
+#####################################################################
+    pushd "${SCRIPT_HOME}/SplunkForwarder" >/dev/null || exit 1
+
+    banner "Building Splunk Universal Forwarder Docker image"
+
+    eval $(minikube docker-env) || exit 1
+    docker --log-level warn build -t ${REGISTRY_CLUSTERIP}/splunk_fwdr_7:v1 . || exit 1
+    docker --log-level warn push ${REGISTRY_CLUSTERIP}/splunk_fwdr_7:v1 || exit 1
+    eval $(minikube docker-env --unset) || exit 1
+
+    popd >/dev/null || exit 1
+}
+
+
+#####################################################################
+function deploy_SplunkUF() {
+#####################################################################
+    pushd "${SCRIPT_HOME}/SplunkForwarder" >/dev/null || exit 1
+
+    banner "Deploying Splunk Universal Forwarder to Minikube (profile: ${MINIKUBE_PROFILE})"
+
+    kubectl apply -f splunk-forwarder-config.yaml || exit 1
+
+    [ -f splunk-forwarder-daemonset.generated.yaml ] && rm splunk-forwarder-daemonset.generated.yaml
+    sed "s/{{REGISTRY_IP}}/${REGISTRY_CLUSTERIP}/g" splunk-forwarder-daemonset.templ.yaml >splunk-forwarder-daemonset.generated.yaml || exit 1
+    kubectl apply -f splunk-forwarder-daemonset.generated.yaml || exit 1
+
+    popd >/dev/null || exit 1
+}
+
+
 #####################################################################
 function deploy_PackagesToMinikube() {
 #####################################################################
     banner "Deploying packages to Minikube (profile: ${MINIKUBE_PROFILE})"
 
-    helm --context=${MINIKUBE_PROFILE} init || exit 1
+    helm --kube-context=${MINIKUBE_PROFILE} init || exit 1
 
     local wordpressPassword
     local mariadbRootPassword
     local mariadbPassword
 
-    read -s -p "Enter value for 'wordpressPassword': " wordpressPassword
-    read -s -p "Enter value for 'mariadbRootPassword': " mariadbRootPassword
-    read -s -p "Enter value for 'mariadbPassword': " mariadbPassword
+    read -s -p "Enter value for 'wordpressPassword': " wordpressPassword; echo
+    read -s -p "Enter value for 'mariadbRootPassword': " mariadbRootPassword; echo
+    read -s -p "Enter value for 'mariadbPassword': " mariadbPassword; echo
 
-    helm --context=${MINIKUBE_PROFILE} install \
+    helm --kube-context=${MINIKUBE_PROFILE} install \
          -f WordPress/values-stable-wordpress-topicteam.yaml \
          stable/wordpress \
          --set-string wordpressPassword="${wordpressPassword}" \
@@ -162,7 +276,15 @@ function deploy_PackagesToMinikube() {
 install_VirtualBox
 install_Vagrant
 install_Kali
+install_kubernetes_cli
+install_Helm
+install_Docker
+start_Docker
+install_Minikube
 createAndRun_Minikube
+wait_until_k8s_environment_is_ready
+wait_minikiube_registry_addon_is_ready
 create_K8s_Namespace
 build_SplunkForwarderImage
-run_SplunkForwarderContainer
+deploy_SplunkUF
+deploy_PackagesToMinikube
